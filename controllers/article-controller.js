01d18efd8f6abffe424dc8ce2c24075492f29e4c
Noelle-KH <noelletang86@gmail.com>
2023-06-20 17:21:02 +0800

    feat: add article crud feature

--- controllers/article-controller.js
+++ controllers/article-controller.js
@@ -1,3 +1,5 @@
+const createError = require('http-errors')
+
 const Article = require('../models/article-model')
 const {
   generateShortenUrl,
@@ -6,6 +8,29 @@ const {
 } = require('../helpers/article-helpers')
 
 const articleController = {
+  getArticles: async (req, res, next) => {
+    try {
+      const articles = await Article.find({}).select('-__v -updatedAt').lean()
+
+      res.json({ articles })
+    } catch (error) {
+      next(error)
+    }
+  },
+  getArticle: async (req, res, next) => {
+    try {
+      const { id } = req.params
+
+      const article = await Article.findById(id)
+        .select('-__v -updatedAt')
+        .lean()
+      if (!article) throw createError.NotFound('Article not found')
+
+      res.json({ article })
+    } catch (error) {
+      next(error)
+    }
+  },
   addArticle: async (req, res, next) => {
     try {
       const userId = req.id
@@ -25,12 +50,49 @@ const articleController = {
         summary,
         userId
       })
-
       const savedArticle = await article.save()
+
       res.json({
         message: 'Create article successfully',
-        id: savedArticle._id
+        article: savedArticle
       })
+    } catch (error) {
+      next(error)
+    }
+  },
+  updateArticle: async (req, res, next) => {
+    try {
+      const userId = req.id
+      const { id } = req.params
+      const { title, summary, record, tagsId } = req.body
+      if (!title || !summary) {
+        throw createError.BadRequest('Title and summary are required')
+      }
+
+      const updatedArticle = await Article.findOneAndUpdate(
+        { _id: id, userId },
+        { title, summary, record, tagsId }
+      )
+      if (!updatedArticle) {
+        throw createError.NotFound('Article not found')
+      }
+
+      res.json({ message: 'Update article successfully' })
+    } catch (error) {
+      next(error)
+    }
+  },
+  deleteArticle: async (req, res, next) => {
+    try {
+      const userId = req.id
+      const { id } = req.params
+
+      const deleteArticle = await Article.findOneAndDelete({ _id: id, userId })
+      if (!deleteArticle) {
+        throw createError.NotFound('Article not found')
+      }
+
+      res.json({ message: 'Delete article successfully' })
     } catch (error) {
       next(error)
     }
--- routes/modules/articles-route.js
+++ routes/modules/articles-route.js
@@ -2,6 +2,11 @@ const router = require('express').Router()
 const articleController = require('../../controllers/article-controller')
 const { articleFieldValidation } = require('../../middleware/validation')
 
+router.get('/:id', articleController.getArticle)
+router.put('/:id', articleController.updateArticle)
+router.delete('/:id', articleController.deleteArticle)
+
+router.get('/', articleController.getArticles)
 router.post('/', articleFieldValidation, articleController.addArticle)
 
 module.exports = router
