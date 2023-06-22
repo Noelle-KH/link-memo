2f4e5a3db7bab7e0ed9e8383030769b2f2e5c79a
Noelle-KH <noelletang86@gmail.com>
2023-06-22 23:34:11 +0800

    feat: add user follow relationship features

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -3,6 +3,7 @@ const createError = require('http-errors')
 const Article = require('../models/article-model')
 const User = require('../models/user-model')
 const Comment = require('../models/comment-model')
+const Follow = require('../models/follow-model')
 
 const userController = {
   getUser: async (req, res, next) => {
@@ -18,6 +19,7 @@ const userController = {
       next(error)
     }
   },
+  updateUser: (req, res, next) => {},
   getUserArticles: async (req, res, next) => {
     try {
       const { id } = req.params
@@ -38,6 +40,66 @@ const userController = {
         .lean()
 
       res.json({ comments })
+    } catch (error) {
+      next(error)
+    }
+  },
+  getUserFollows: async (req, res, next) => {
+    try {
+      const { id } = req.params
+      const followShip = await Follow.find({
+        $or: [{ followerId: id }, { followingId: id }]
+      })
+        .select('-__v')
+        .lean()
+
+      res.json({ followShip })
+    } catch (error) {
+      next(error)
+    }
+  },
+  postUserFollow: async (req, res, next) => {
+    try {
+      const followerId = req.id
+      const followingId = req.params.id
+      console.log(followerId === followingId)
+      if (followerId === followingId) {
+        throw createError.BadRequest('Cannot follow yourself')
+      }
+
+      const followExist = await Follow.findOne({ followerId, followingId })
+      if (followExist) {
+        throw createError.BadRequest('You are already following this user')
+      }
+
+      await Follow.create({ followerId, followingId })
+
+      res.json({
+        message: 'Create new following relationship successfully'
+      })
+    } catch (error) {
+      next(error)
+    }
+  },
+  deleteUserFollow: async (req, res, next) => {
+    try {
+      const followerId = req.id
+      const followingId = req.params.id
+      if (followerId === followingId) {
+        throw createError.BadRequest('Cannot follow yourself')
+      }
+
+      const followExist = await Follow.findOneAndDelete({
+        followerId,
+        followingId
+      })
+      if (!followExist) {
+        throw createError.BadRequest('You are not following this user')
+      }
+
+      res.json({
+        message: 'Delete following relationship successfully'
+      })
     } catch (error) {
       next(error)
     }
--- routes/modules/users-route.js
+++ routes/modules/users-route.js
@@ -2,8 +2,14 @@ const router = require('express').Router()
 
 const userController = require('../../controllers/user-controller')
 
+router.get('/:id/follows', userController.getUserFollows)
+router.post('/:id/follows', userController.postUserFollow)
+router.delete('/:id/follows', userController.deleteUserFollow)
+
 router.get('/:id/articles', userController.getUserArticles)
+
 router.get('/:id/comments', userController.getUserComments)
+
 router.get('/:id', userController.getUser)
 
 module.exports = router
